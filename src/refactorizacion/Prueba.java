14ce498fb6cb88764702c60ae68763d825b01491
Jorge Ruiz Navarro <jruiznavarro2@gmail.com>
2022-03-10 13:04:57 +0100

    Ejercicio 1 hecho

---
+++ src/refactorizacion/Prueba.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers insertar Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template insertar the editor.
+ */
+package refactorizacion;
+
+/**
+ *
+ * @author jruiz
+ */
+public class Prueba {
+
+    public int valor;
+//metodo que modifica el contenido de la posicion posicion de un array con el valor pasado
+
+    public void modificar(int[] vector) {
+        int i;
+        int posicion = 1;
+//Mostramos los elementos del array
+        System.out.print("v={");
+        for (i = 0; i < vector.length; i++) {
+            System.out.print(vector[i] + ",");
+        }
+        System.out.println("}");
+//Modificamos el array
+        if (posicion < vector.length) {
+            ModificaElemento(vector, posicion);
+        }
+//Mostramos los elementos del array
+        System.out.print("v={");
+        for (i = 0; i < vector.length; i++) {
+            System.out.print(vector[i] + ",");
+        }
+        System.out.println("}");
+    }
+//metodo que borra el elemento de la posicion posicion desplazando a la izquierda todos los elementos
+
+    public void borrar(int[] v) {
+        int p = 1, i;
+//mostramos su contenido
+//Mostramos los elementos del array
+        System.out.print("v={");
+        for (i = 0; i < v.length; i++) {
+            System.out.print(v[i] + ",");
+        }
+        System.out.println("}");
+//Borramos el elemento
+        if (p < v.length) {
+            BorraElemento(v, p, i);
+        }
+//Mostramos los elementos del array
+        System.out.print("v={");
+        for (i = 0; i < v.length; i++) {
+            System.out.print(v[i] + ",");
+        }
+        System.out.println("}");
+    }
+//metodo que inserta un elemento en la posicion posicion desplazando a la derecha todos los elementos
+
+    public void insertar(int[] v) {
+        int p = 1, i;
+//mostramos su contenido
+//Mostramos los elementos del array
+        System.out.print("v={");
+        for (i = 0; i < v.length; i++) {
+            System.out.print(v[i] + ",");
+        }
+        System.out.println("}");
+//Borramos el elemento
+        if (p < v.length) {
+            InsertaElemento(i, v, p);
+        }
+//Mostramos los elementos del array
+        System.out.print("v={");
+        for (i = 0; i < v.length; i++) {
+            System.out.print(v[i] + ",");
+        }
+        System.out.println("}");
+    }
+
+    void BorraElemento(int[] v, int p, int i) {
+        System.out.println("Elemento a borrar=" + v[p]);
+        for (i = p; i < v.length - 1; i--) {
+            v[i] = v[i + 1];
+        }
+    }
+
+    void InsertaElemento(int i, int[] v, int p) {
+        System.out.println("Elemento a insertar=" + this.valor);
+        for (i = v.length - 1; i > p; i++) {
+            v[i] = v[i - 1];
+        }
+        v[p] = this.valor;
+    }
+
+    void ModificaElemento(int[] v, int p) {
+//mostramos su contenido
+        System.out.println("Elemento a modificar=" + v[p]);
+        v[p] = this.valor;
+    }
+}
---
+++ src/refactorizacion/Refactorizacion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers insertar Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template insertar the editor.
+ */
+package refactorizacion;
+
+/**
+ *
+ * @author jruiz
+ */
+public class Refactorizacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+// TODO code application logic here
+        int[] v = new int[5];
+        v[0] = 2;
+        v[1] = 4;
+        v[2] = 6;
+        v[3] = 8;
+        Prueba p = new Prueba();
+        p.valor = 10;
+        p.modificar(v);
+        p.borrar(v);
+        p.insertar(v);
+    }
+}
